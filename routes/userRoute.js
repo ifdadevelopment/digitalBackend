1eb767f9da09119aa94ce93a27d9b9c91ecb647e
ifdadevelopment <ifdadevelopment.team@gmail.com>
2025-06-30 19:07:58 +0530

    addUrl

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { registerUser, loginUser, updateUserProfile, logoutUser } from "../controllers/userController.js";
 import verifyUser from "../middleware/auth.js";
-
+//route to handle user registration, login, profile update, and logout  
 const userRouter = express.Router();
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
