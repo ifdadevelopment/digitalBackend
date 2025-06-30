3ae85f58f87e810ec7979e3e5b37cb7b10cb5e57
Aditya Kumar Mishra <89987516+Adityamishra114@users.noreply.github.com>
2025-06-30 23:51:18 +0530

    addChanges

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { registerUser, loginUser, updateUserProfile, logoutUser } from "../controllers/userController.js";
-import verifyUser from "../middleware/auth.js";
-//route to handle user registration, login, profile update, and logout  
+import { registerUser, loginUser, updateUserProfile,getUserProfile, logoutUser } from "../controllers/userController.js";
+import verifyUser from "../middleware/auth.js"; 
 const userRouter = express.Router();
 userRouter.post("/register", registerUser);
+userRouter.post("/me",verifyUser, getUserProfile);
 userRouter.post("/login", loginUser);
 userRouter.put("/update-profile", verifyUser, updateUserProfile);
 userRouter.delete("/logout", logoutUser);
