eed2b72336f19d1840638782b99ba6f36e17bd63
ifdadevelopment <ifdadevelopment.team@gmail.com>
2025-07-08 16:24:35 +0530

    addChange

--- models/Blog.js
+++ models/Blog.js
@@ -82,4 +82,5 @@ const blogSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-export default mongoose.model("Blog", blogSchema);
+const Blog = mongoose.model("Blog", blogSchema);
+export default Blog;
