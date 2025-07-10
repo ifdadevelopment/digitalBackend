c47c34a1add19aeb1c54c0ffa1801c95a485f0d8
ifdadevelopment <ifdadevelopment.team@gmail.com>
2025-07-10 18:41:11 +0530

    addChanges

--- server.js
+++ server.js
@@ -34,7 +34,8 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:4173",
-  "https://digitalmarketing890.netlify.app"
+  "https://digitalmarketing890.netlify.app",
+  "https://digitaladmin.netlify.app"
 ];
 
 app.use(
